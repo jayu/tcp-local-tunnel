4b5fb02fd450dac179f0e6717820e23096297087
João Duarte <du@rte.pm>
2020-11-18 14:25:41 +0000

    added support for transport AES encryption

--- example/localServer.js
+++ example/localServer.js
@@ -1,47 +1,15 @@
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
-
 const { client } = require('../index.js');
-
-/* simple express server showcase */
-
-const serverPort = 3000;
-
-const server = express();
-
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded());
-
-server.get('/someurl', function(req, res, next) {
-  res.send('some url page');
-});
-server.get('/video.mp4', (req, res) => {
-  res.header('Content-Type', 'video/mp4');
-  fs.createReadStream('neverGonna.mp4').pipe(res);
-});
-server.get('/long.json', (req, res) => {
-  const file = fs.readFileSync('long.json');
-  res.header('Content-Length', Buffer.byteLength(file));
-  console.log('contetn length', Buffer.byteLength(file));
-  res.json(file);
-});
-server.get('/', function(req, res, next) {
-  res.send('Main page');
-});
-
-server.listen(serverPort);
-
-/* tcp tunnel config */
 
 client(
   {
-    host: '255.255.255.255',
-    port: 8010
+    host: '127.0.0.1', //REMOTE
+    port: 8010,
+    // Uncomment below to enable transport encryption::
+    // encKey: 'DwUuKDoFtHVWrWGfS4rz1pm7bOTZ988o',
+    // encIv: '435435f432543532'
   },
   {
-    host: 'localhost',
-    port: serverPort
-  },
-  40
+    host: '127.0.0.1', //LOCAL
+    port: 1234
+  }
 );
